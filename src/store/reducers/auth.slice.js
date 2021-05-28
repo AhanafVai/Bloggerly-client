ea82216234975814c342ff64d14882ae84247991
shohaul00 <shihabshohaul143@gmail.com>
2021-05-28 21:41:01 +0600

    issue resolved - bootstrap cdn javascript

--- src/store/reducers/auth.slice.js
+++ src/store/reducers/auth.slice.js
@@ -1,21 +1,21 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit"
 
 const authSlice = createSlice({
   name: "auth",
   initialState: { userData: null, isSignedIn: false },
   reducers: {
     setSignedIn: (state, action) => {
-      state.isSignedIn = action.payload;
+      state.isSignedIn = action.payload
     },
     setUserData: (state, action) => {
-      state.userData = action.payload;
+      state.userData = action.payload
     },
   },
-});
+})
 
-export const { setSignedIn, setUserData } = authSlice.actions;
+export const { setSignedIn, setUserData } = authSlice.actions
 
-export const selectSignedIn = (state) => state.auth.isSignedIn;
-export const selectUserData = (state) => state.auth.userData;
+export const selectSignedIn = state => state.auth.isSignedIn
+export const selectUserData = state => state.auth.userData
 
-export default authSlice.reducer;
+export default authSlice.reducer
